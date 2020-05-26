5a926918b53a17785d4fb54a5f51fbb7bf92d583
James Hamilton <james.hamilton@guardsquare.com>
2020-05-26 12:18:25 +0200

    Add ClassProcessingInfoFilter

---
+++ src/proguard/classfile/visitor/ClassProcessingInfoFilter.java
@@ -0,0 +1,80 @@
+/*
+ * ProGuard -- shrinking, optimization, obfuscation, and preverification
+ *             of Java bytecode.
+ *
+ * Copyright (c) 2002-2020 Guardsquare NV
+ */
+package proguard.classfile.visitor;
+
+import proguard.classfile.Clazz;
+import proguard.util.Processable;
+
+import java.util.function.Predicate;
+
+/**
+ * Delegates all class visits to another given visitor, depending on if the given predicate passes or not.
+ *
+ * @author Johan Leys
+ * @author James Hamilton
+ */
+public class ClassProcessingInfoFilter
+implements ClassVisitor
+{
+
+    private final Predicate<Object> predicate;
+    private final ClassVisitor acceptedClassVisitor;
+    private final ClassVisitor rejectedClassVisitor;
+
+
+    /**
+     * Creates a new  ClassVisitorInfoFilter.
+     * @param predicate            the visitor info predicate to check.
+     * @param acceptedClassVisitor the class visitor for classes that have the
+     *                             given visitor info.
+     */
+    public ClassProcessingInfoFilter(Predicate<Object> predicate,
+                                     ClassVisitor acceptedClassVisitor)
+    {
+        this(predicate, acceptedClassVisitor, null);
+    }
+
+
+    /**
+     * Creates a new  ClassVisitorInfoFilter that checks the identity of the
+     * given visitor info.
+     * @param predicate            the visitor info to check.
+     * @param acceptedClassVisitor the class visitor for classes that have the
+     *                             given visitor info.
+     * @param rejectedClassVisitor the class visitor for classes that don't
+     *                             have the given visitor info.
+     */
+    public ClassProcessingInfoFilter(Predicate<Object> predicate,
+                                     ClassVisitor acceptedClassVisitor,
+                                     ClassVisitor rejectedClassVisitor)
+    {
+        this.predicate            = predicate;
+        this.acceptedClassVisitor = acceptedClassVisitor;
+        this.rejectedClassVisitor = rejectedClassVisitor;
+    }
+
+
+    // Implementations for ClassVisitor.
+
+    @Override
+    public void visitAnyClass(Clazz clazz)
+    {
+        ClassVisitor delegate = this.getDelegate(clazz);
+        if (delegate != null)
+        {
+            clazz.accept(delegate);
+        }
+    }
+
+
+    // Helper methods.
+
+    private ClassVisitor getDelegate(Processable processable)
+    {
+        return this.predicate.test(processable.getProcessingInfo()) ? this.acceptedClassVisitor : this.rejectedClassVisitor;
+    }
+}
