9d6d62d9c742b40a189b991d358bc07161aeef82
Toon Willemot <toon.willemot@guardsquare.com>
2021-02-22 15:47:58 +0100

    fix lexical import order for PartialEvaluatorHelper

--- test/unit/src/proguard/util/PartialEvaluatorHelper.kt
+++ test/unit/src/proguard/util/PartialEvaluatorHelper.kt
@@ -13,11 +13,10 @@ import proguard.classfile.util.ClassUtil
 import proguard.evaluation.ExecutingInvocationUnit
 import proguard.evaluation.PartialEvaluator
 import proguard.evaluation.value.ArrayReferenceValueFactory
+import proguard.evaluation.value.ParticularReferenceValue
 import proguard.evaluation.value.ParticularValueFactory
 import proguard.evaluation.value.Value
 import proguard.evaluation.value.ValueFactory
-import proguard.evaluation.value.ParticularReferenceValue
-
 
 class PartialEvaluatorHelper {
     companion object {
