67a35fe1aae931f70ab3ad725e8cc8e03df54e78
James Hamilton <james.hamilton@guardsquare.com>
2021-06-22 11:04:00 +0200

    Add Kotlin Annotation ClassReferenceFixer test

---
+++ src/test/java/testutils/classfile/util/ClassRenamer.java
@@ -0,0 +1,182 @@
+/*
+ * ProGuardCORE -- library to process Java bytecode.
+ *
+ * Copyright (c) 2002-2020 Guardsquare NV
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package testutils.classfile.util;
+
+import proguard.classfile.*;
+import proguard.classfile.constant.ClassConstant;
+import proguard.classfile.constant.visitor.ConstantVisitor;
+import proguard.classfile.editor.ConstantPoolEditor;
+import proguard.classfile.visitor.ClassVisitor;
+import proguard.classfile.visitor.MemberVisitor;
+
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+
+/**
+ * This <code>ClassVisitor</code> renames the class names and class member
+ * names of the classes it visits.
+ *
+ * // TODO(T5302): Move class to ProGuardCORE API.
+ *
+ * @author Eric Lafortune
+ */
+public class ClassRenamer
+implements   ClassVisitor,
+             MemberVisitor,
+             ConstantVisitor
+{
+    private final Function<Clazz, String>           classNameFunction;
+    private final BiFunction<Clazz, Member, String> memberNameFunction;
+    private final ClassVisitor                      extraClassVisitor;
+    private final MemberVisitor                     extraMemberVisitor;
+
+
+    public ClassRenamer(Function<Clazz, String> classNameFunction)
+    {
+        this(classNameFunction, (clazz, member) -> member.getName(clazz), null, null);
+    }
+
+
+    public ClassRenamer(Function<Clazz, String> classNameFunction, BiFunction<Clazz, Member, String> memberNameFunction)
+    {
+        this(classNameFunction, memberNameFunction, null, null);
+    }
+
+
+    public ClassRenamer(Function<Clazz, String> classNameFunction,
+                        ClassVisitor            extraClassVisitor,
+                        MemberVisitor           extraMemberVisitor)
+    {
+        this(classNameFunction, (clazz, member) -> member.getName(clazz), extraClassVisitor, extraMemberVisitor);
+    }
+
+
+    public ClassRenamer(Function<Clazz, String>           classNameFunction,
+                        BiFunction<Clazz, Member, String> memberNameFunction,
+                        ClassVisitor                      extraClassVisitor,
+                        MemberVisitor                     extraMemberVisitor)
+    {
+        this.classNameFunction  = classNameFunction;
+        this.memberNameFunction = memberNameFunction;
+        this.extraClassVisitor  = extraClassVisitor;
+        this.extraMemberVisitor = extraMemberVisitor;
+    }
+
+
+    // Implementations for ClassVisitor.
+
+    @Override
+    public void visitAnyClass(Clazz clazz)
+    {
+        throw new UnsupportedOperationException(this.getClass().getName() + " does not support " + clazz.getClass().getName());
+    }
+
+
+    @Override
+    public void visitProgramClass(ProgramClass programClass)
+    {
+        // Rename this class.
+        programClass.thisClassConstantAccept(this);
+
+        // Rename the class members.
+        programClass.fieldsAccept(this);
+        programClass.methodsAccept(this);
+    }
+
+
+    @Override
+    public void visitLibraryClass(LibraryClass libraryClass)
+    {
+        // Has the library class name changed?
+        String name    = libraryClass.getName();
+        String newName = this.classNameFunction.apply(libraryClass);
+        if (newName != null && !newName.equals(name))
+        {
+            libraryClass.thisClassName = newName;
+
+            if (extraClassVisitor != null)
+            {
+                extraClassVisitor.visitLibraryClass(libraryClass);
+            }
+        }
+
+        // Rename the class members.
+        libraryClass.fieldsAccept(this);
+        libraryClass.methodsAccept(this);
+    }
+
+
+    // Implementations for MemberVisitor.
+
+    public void visitProgramMember(ProgramClass  programClass,
+                                   ProgramMember programMember)
+    {
+        // Has the class member name changed?
+        String name    = programMember.getName(programClass);
+        String newName = memberNameFunction.apply(programClass, programMember);
+        if (newName != null && !newName.equals(name))
+        {
+            programMember.u2nameIndex =
+                new ConstantPoolEditor(programClass).addUtf8Constant(newName);
+
+            if (extraMemberVisitor != null)
+            {
+                programMember.accept(programClass, extraMemberVisitor);
+            }
+        }
+    }
+
+    public void visitLibraryMember(LibraryClass  libraryClass,
+                                   LibraryMember libraryMember)
+    {
+        // Has the library member name changed?
+        String name    = libraryMember.getName(libraryClass);
+        String newName = memberNameFunction.apply(libraryClass, libraryMember);
+        if (newName != null && !newName.equals(name))
+        {
+            libraryMember.name = newName;
+
+            if (extraMemberVisitor != null)
+            {
+                libraryMember.accept(libraryClass, extraMemberVisitor);
+            }
+        }
+    }
+
+
+    // Implementations for ConstantVisitor.
+
+    public void visitClassConstant(Clazz clazz, ClassConstant classConstant)
+    {
+        // Update the Class entry if required.
+        String name    = clazz.getName();
+        String newName = classNameFunction.apply(clazz);
+        if (newName != null && !newName.equals(name))
+        {
+            // Refer to a new Utf8 entry.
+            classConstant.u2nameIndex =
+                new ConstantPoolEditor((ProgramClass)clazz).addUtf8Constant(newName);
+
+            if (extraClassVisitor != null)
+            {
+                clazz.accept(extraClassVisitor);
+            }
+        }
+    }
+}
--- src/test/kotlin/proguard/classfile/editor/ClassReferenceFixerTest.kt
+++ src/test/kotlin/proguard/classfile/editor/ClassReferenceFixerTest.kt
@@ -2,9 +2,23 @@ package proguard.classfile.editor
 
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.shouldBe
+import io.mockk.slot
+import io.mockk.spyk
+import io.mockk.verify
 import proguard.classfile.AccessConstants.PUBLIC
 import proguard.classfile.ClassConstants.NAME_JAVA_LANG_OBJECT
 import proguard.classfile.editor.ClassReferenceFixer.shortKotlinNestedClassName
+import proguard.classfile.kotlin.KotlinMetadataAnnotation
+import proguard.classfile.kotlin.KotlinTypeMetadata
+import proguard.classfile.kotlin.visitor.AllKotlinAnnotationVisitor
+import proguard.classfile.kotlin.visitor.KotlinAnnotationVisitor
+import proguard.classfile.kotlin.visitor.ReferencedKotlinMetadataVisitor
+import proguard.classfile.visitor.AllMethodVisitor
+import proguard.classfile.visitor.ClassNameFilter
+import proguard.classfile.visitor.MultiClassVisitor
+import testutils.ClassPoolBuilder
+import testutils.KotlinSource
+import testutils.classfile.util.ClassRenamer
 
 class ClassReferenceFixerTest : FreeSpec({
     "Kotlin nested class short names should be generated correctly" - {
@@ -32,6 +46,130 @@ class ClassReferenceFixerTest : FreeSpec({
         "when they have a new name with a package" {
             val referencedClass = ClassBuilder(55, PUBLIC, "mypackage/newOuterClass\$newInnerClass", NAME_JAVA_LANG_OBJECT).programClass
             shortKotlinNestedClassName("OuterClass", "innerClass", referencedClass) shouldBe "newInnerClass"
+        }
+    }
+
+    "Kotlin annotations should be fixed correctly" - {
+        val programClassPool = ClassPoolBuilder.fromSource(
+            KotlinSource(
+                "Test.kt",
+                """
+                import kotlin.reflect.KClass
+
+                @Target(AnnotationTarget.TYPE)
+                annotation class MyTypeAnnotation(
+                    val string: String,
+                    val byte: Byte,
+                    val char: Char,
+                    val short: Short,
+                    val int: Int,
+                    val long: Long,
+                    val float: Float,
+                    val double: Double,
+                    val boolean: Boolean,
+                    val uByte: UByte,
+                    val uShort: UShort,
+                    val uInt: UInt,
+                    val uLong: ULong,
+                    val kClass: KClass<*>,
+                    val enum: MyEnum,
+                    val array: Array<String>,
+                    val annotation: Foo
+                )
+
+                val x: @MyTypeAnnotation(
+                    string = "foo",
+                    byte = 1,
+                    char = 'a',
+                    short = 1,
+                    int = 1,
+                    long = 1L,
+                    float = 1f,
+                    double = 1.0,
+                    boolean = true,
+                    uByte = 1u,
+                    uShort = 1u,
+                    uInt = 1u,
+                    uLong = 1uL,
+                    kClass = String::class,
+                    enum = MyEnum.FOO,
+                    array = arrayOf("foo", "bar"),
+                    annotation = Foo("foo")) String = "foo"
+
+                // extra helpers
+
+                enum class MyEnum { FOO, BAR }
+                annotation class Foo(val string: String)
+                """.trimIndent()
+            ),
+            kotlincArguments = listOf("-Xuse-experimental=kotlin.ExperimentalUnsignedTypes")
+        )
+
+        with(programClassPool) {
+            classesAccept(
+                MultiClassVisitor(
+                    ClassRenamer {
+                        when (it.name) {
+                            "MyTypeAnnotation" -> "MyRenamedTypeAnnotation"
+                            "MyEnum" -> "MyRenamedEnum"
+                            "Foo" -> "RenamedFoo"
+                            else -> it.name
+                        }
+                    },
+                    // Rename all the methods in the annotation class
+                    ClassNameFilter(
+                        "MyRenamedTypeAnnotation",
+                        AllMethodVisitor(
+                            ClassRenamer(
+                                { it.name },
+                                { clazz, member -> "renamed${member.getName(clazz).capitalize()}" }
+                            )
+                        )
+                    )
+                )
+            )
+
+            // The ClassReferenceFixer should rename everything correctly
+            classesAccept(ClassReferenceFixer(false))
+        }
+
+        val annotationVisitor = spyk<KotlinAnnotationVisitor>()
+        val fileFacadeClass = programClassPool.getClass("TestKt")
+
+        programClassPool.classesAccept(ReferencedKotlinMetadataVisitor(AllKotlinAnnotationVisitor(annotationVisitor)))
+        val annotation = slot<KotlinMetadataAnnotation>()
+
+        "there should be 1 annotation visited" {
+            verify(exactly = 1) { annotationVisitor.visitTypeAnnotation(fileFacadeClass, ofType(KotlinTypeMetadata::class), capture(annotation)) }
+        }
+
+        "the annotation class name should be correctly renamed" {
+            annotation.captured.kmAnnotation.className shouldBe "MyRenamedTypeAnnotation"
+            annotation.captured.referencedAnnotationClass shouldBe programClassPool.getClass("MyTypeAnnotation")
+        }
+
+        "the annotation field references should be correctly set" {
+            with(programClassPool.getClass("MyTypeAnnotation")) {
+                annotation.captured.referencedArgumentMethods shouldBe mapOf(
+                    "renamedString" to findMethod("renamedString", "()Ljava/lang/String;"),
+                    "renamedByte" to findMethod("renamedByte", "()B"),
+                    "renamedChar" to findMethod("renamedChar", "()C"),
+                    "renamedShort" to findMethod("renamedShort", "()S"),
+                    "renamedInt" to findMethod("renamedInt", "()I"),
+                    "renamedLong" to findMethod("renamedLong", "()J"),
+                    "renamedFloat" to findMethod("renamedFloat", "()F"),
+                    "renamedDouble" to findMethod("renamedDouble", "()D"),
+                    "renamedBoolean" to findMethod("renamedBoolean", "()Z"),
+                    "renamedUByte" to findMethod("renamedUByte", "()B"),
+                    "renamedUShort" to findMethod("renamedUShort", "()S"),
+                    "renamedUInt" to findMethod("renamedUInt", "()I"),
+                    "renamedULong" to findMethod("renamedULong", "()J"),
+                    "renamedEnum" to findMethod("renamedEnum", "()LMyRenamedEnum;"),
+                    "renamedArray" to findMethod("renamedArray", "()[Ljava/lang/String;"),
+                    "renamedAnnotation" to findMethod("renamedAnnotation", "()LRenamedFoo;"),
+                    "renamedKClass" to findMethod("renamedKClass", "()Ljava/lang/Class;")
+                )
+            }
         }
     }
 })
