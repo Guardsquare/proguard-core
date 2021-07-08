889bdb324d0d181441417c576ccc313b7ba7c792
James Hamilton <james.hamilton@guardsquare.com>
2021-07-08 17:55:35 +0200

    Change types to non-nullable in ConstantInstructionTest

--- src/test/kotlin/proguard/classfile/instruction/ConstantInstructionTest.kt
+++ src/test/kotlin/proguard/classfile/instruction/ConstantInstructionTest.kt
@@ -1,8 +1,19 @@
 /*
- * ProGuard -- shrinking, optimization, obfuscation, and preverification
- *             of Java bytecode.
+ * ProGuardCORE -- library to process Java bytecode.
  *
  * Copyright (c) 2002-2021 Guardsquare NV
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package proguard.classfile.instruction
@@ -45,9 +56,9 @@ class ConstantInstructionTest : FreeSpec({
                 AllAttributeVisitor(
                     AllInstructionVisitor(
                         object : InstructionVisitor {
-                            override fun visitAnyInstruction(clazz: Clazz?, method: Method?, codeAttribute: CodeAttribute?, offset: Int, instruction: Instruction?) { }
-                            override fun visitConstantInstruction(clazz: Clazz?, method: Method?, codeAttribute: CodeAttribute?, offset: Int, constantInstruction: ConstantInstruction?) {
-                                if (constantInstruction?.opcode == OP_LDC && constantInstruction.mayInstanceThrowExceptions(clazz)) throwingLdcCount++
+                            override fun visitAnyInstruction(clazz: Clazz, method: Method, codeAttribute: CodeAttribute, offset: Int, instruction: Instruction) { }
+                            override fun visitConstantInstruction(clazz: Clazz, method: Method, codeAttribute: CodeAttribute, offset: Int, constantInstruction: ConstantInstruction) {
+                                if (constantInstruction.opcode == OP_LDC && constantInstruction.mayInstanceThrowExceptions(clazz)) throwingLdcCount++
                             }
                         })
                 )
@@ -79,9 +90,9 @@ class ConstantInstructionTest : FreeSpec({
                 AllAttributeVisitor(
                     AllInstructionVisitor(
                         object : InstructionVisitor {
-                            override fun visitAnyInstruction(clazz: Clazz?, method: Method?, codeAttribute: CodeAttribute?, offset: Int, instruction: Instruction?) { }
-                            override fun visitConstantInstruction(clazz: Clazz?, method: Method?, codeAttribute: CodeAttribute?, offset: Int, constantInstruction: ConstantInstruction?) {
-                                if (constantInstruction?.opcode == OP_LDC && constantInstruction.mayInstanceThrowExceptions(clazz)) throwingLdcCount++
+                            override fun visitAnyInstruction(clazz: Clazz, method: Method, codeAttribute: CodeAttribute, offset: Int, instruction: Instruction) { }
+                            override fun visitConstantInstruction(clazz: Clazz, method: Method, codeAttribute: CodeAttribute, offset: Int, constantInstruction: ConstantInstruction) {
+                                if (constantInstruction.opcode == OP_LDC && constantInstruction.mayInstanceThrowExceptions(clazz)) throwingLdcCount++
                             }
                         })
                 )
