329b0cbf5ee0441ae36ab11dc4c90d0884816ac2
Ruben Pieters <ruben.pieters@guardsquare.com>
2021-10-11 16:56:14 +0200

    Add MethodCounter

---
+++ src/main/java/proguard/classfile/visitor/MethodCounter.java
@@ -0,0 +1,42 @@
+/*
+ * ProGuard -- shrinking, optimization, obfuscation, and preverification
+ *             of Java bytecode.
+ *
+ * Copyright (c) 2002-2021 Guardsquare NV
+ */
+
+package proguard.classfile.visitor;
+
+import proguard.classfile.ProgramClass;
+import proguard.classfile.ProgramMethod;
+import proguard.util.Counter;
+
+/**
+ * This {@link MemberVisitor} counts the number of methods that have been visited.
+ *
+ * @author Ruben Pieters
+ */
+public class MethodCounter
+implements   MemberVisitor,
+             Counter
+{
+    private int count;
+
+
+    // Implementations for Counter.
+
+    @Override
+    public int getCount()
+    {
+        return count;
+    }
+
+
+    // Implementations for MemberVisitor.
+
+    @Override
+    public void visitProgramMethod(ProgramClass programClass, ProgramMethod programMethod)
+    {
+        count++;
+    }
+}
