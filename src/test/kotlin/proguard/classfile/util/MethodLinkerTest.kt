5d6a47b5723370573b8ce6ad303da5c24edc9282
Emiel Vandeloo <emiel.vandeloo@guardsquare.com>
2022-02-19 19:48:02 +0100

    Prevent linking a final method that is shadowed in a subclass

---
+++ src/test/kotlin/proguard/classfile/util/MethodLinkerTest.kt
@@ -0,0 +1,46 @@
+/*
+ * ProGuard -- shrinking, optimization, obfuscation, and preverification
+ *             of Java bytecode.
+ *
+ * Copyright (c) 2002-2022 Guardsquare NV
+ */
+
+package proguard.classfile.util
+
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
+import testutils.ClassPoolBuilder
+import testutils.JavaSource
+
+class MethodLinkerTest : FreeSpec ({
+    "Given a super class with a package-private final method that is also defined in a subclass" - {
+        val (programClassPool, _) = ClassPoolBuilder.fromSource(
+            JavaSource(
+                "Super.java",
+                """
+                    package a;
+                    public class Super {
+                        final void foo() { }
+                    }
+                """.trimIndent()
+            ),
+            JavaSource(
+                "Foo.java",
+                """
+                    public class Foo extends a.Super {
+                        public final void foo() { }
+                    }
+                """.trimIndent()
+            )
+        )
+
+        "When the subclass is repackaged" - {
+            programClassPool.getClass("Foo").accept(ClassRenamer { "a/Foo" })
+
+            "Then the super-method is not linked to the sub-method" {
+                programClassPool.classesAccept(MethodLinker())
+                programClassPool.getClass("Foo").findMethod("foo", null).processingInfo shouldBe null
+            }
+        }
+    }
+})
