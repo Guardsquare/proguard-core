bf8155b1ae3992d97a8d0015088a8b7b1e7546b4
Joren Van Hecke <joren.vanhecke@student.kuleuven.be>
2022-03-27 22:24:50 +0200

    FieldRenamer: use descriptor + index as new name

--- src/main/java/proguard/classfile/visitor/FieldRenamer.java
+++ src/main/java/proguard/classfile/visitor/FieldRenamer.java
@@ -1,21 +1,35 @@
 package proguard.classfile.visitor;
 
 import proguard.classfile.Clazz;
+import proguard.classfile.Field;
 import proguard.classfile.ProgramClass;
 import proguard.classfile.ProgramField;
 import proguard.classfile.constant.Constant;
 import proguard.classfile.constant.Utf8Constant;
 import proguard.classfile.constant.visitor.ConstantVisitor;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class FieldRenamer implements MemberVisitor, ConstantVisitor {
 
     private final String newFieldNamePrefix;
-    private int newFieldNameIndex;
+    private int newFieldNameIndex = 0;
+    private final boolean useDescriptorBasedNames;
+    private final Map<String, Integer> descriptorIndex = new HashMap<>();
+    private Field lastVisitedField;
+    private Clazz lastVisitedClass;
 
     public FieldRenamer(String newFieldNamePrefix)
     {
-        this.newFieldNamePrefix = newFieldNamePrefix;
-        this.newFieldNameIndex  = 0;
+        this.newFieldNamePrefix      = newFieldNamePrefix;
+        this.useDescriptorBasedNames = false;
+    }
+
+    public FieldRenamer(boolean useDescriptorBasedNames)
+    {
+        this.newFieldNamePrefix      = "";
+        this.useDescriptorBasedNames = useDescriptorBasedNames;
     }
 
     public void resetIndex()
@@ -26,6 +40,8 @@ public class FieldRenamer implements MemberVisitor, ConstantVisitor {
     @Override
     public void visitProgramField(ProgramClass programClass, ProgramField programField)
     {
+        this.lastVisitedClass = programClass;
+        this.lastVisitedField = programField;
         programClass.constantPoolEntryAccept(programField.u2nameIndex, this);
     }
 
@@ -35,12 +51,30 @@ public class FieldRenamer implements MemberVisitor, ConstantVisitor {
     @Override
     public void visitUtf8Constant(Clazz clazz, Utf8Constant utf8Constant)
     {
+        String newName = getNextFieldName();
+        utf8Constant.setString(newName);
         this.newFieldNameIndex++;
-        utf8Constant.setString(this.newFieldNamePrefix + this.newFieldNameIndex);
+        String descriptor = this.lastVisitedField.getDescriptor(this.lastVisitedClass);
+        this.descriptorIndex.put(descriptor, this.descriptorIndex.getOrDefault(descriptor, 0) + 1);
+
     }
 
     public String getNextFieldName()
     {
-        return this.newFieldNamePrefix + (this.newFieldNameIndex + 1);
+        String newName;
+        if (useDescriptorBasedNames)
+        {
+            // This is non-logical behaviour: the method name suggests a globally correct next name would be
+            // returned, but here it depends on the previously visited field, while in practice
+            // we don't know whether the next field will have the same descriptor
+            String descriptor = this.lastVisitedField.getDescriptor(this.lastVisitedClass);
+            newName = descriptor.replace(";", "").replace("/", "").replace("[", "") + this.descriptorIndex.getOrDefault(descriptor, 0);
+
+        }
+        else
+        {
+            newName = this.newFieldNamePrefix + (this.newFieldNameIndex + 1);
+        }
+        return newName;
     }
 }
