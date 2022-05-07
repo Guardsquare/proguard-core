8c8279695892bbcffc736e0bc0ecdde73960d887
Joren Van Hecke <joren.vanhecke@student.kuleuven.be>
2022-05-07 15:36:09 +0200

    ClassConstantToClassVisitor: don't visit referenced class if it is null

--- src/main/java/proguard/classfile/attribute/visitor/ClassConstantToClassVisitor.java
+++ src/main/java/proguard/classfile/attribute/visitor/ClassConstantToClassVisitor.java
@@ -2,6 +2,7 @@ package proguard.classfile.attribute.visitor;
 
 import proguard.classfile.Clazz;
 import proguard.classfile.constant.ClassConstant;
+import proguard.classfile.constant.Constant;
 import proguard.classfile.constant.visitor.ConstantVisitor;
 import proguard.classfile.visitor.ClassVisitor;
 
@@ -14,9 +15,11 @@ public class ClassConstantToClassVisitor implements ConstantVisitor {
         this.classVisitor = classVisitor;
     }
 
+    public void visitAnyConstant(Clazz clazz, Constant constant) {}
+
     @Override
     public void visitClassConstant(Clazz clazz, ClassConstant classConstant) {
-        if (this.classVisitor != null)
+        if (this.classVisitor != null && classConstant.referencedClass != null)
         {
             classConstant.referencedClass.accept(this.classVisitor);
         }
