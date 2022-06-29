7441f81326179164c6092731b495659a113f1f6b
Joren Van Hecke <joren.vanhecke@student.kuleuven.be>
2022-06-29 16:43:22 +0200

    Move InnerClassRemover to proguard.classfile.editor + update docs

--- src/main/java/proguard/classfile/editor/InnerClassRemover.java
+++ src/main/java/proguard/classfile/editor/InnerClassRemover.java
@@ -1,4 +1,21 @@
-package proguard.classfile.visitor;
+/*
+ * ProGuardCORE -- library to process Java bytecode.
+ *
+ * Copyright (c) 2002-2022 Guardsquare NV
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package proguard.classfile.editor;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -8,12 +25,17 @@ import proguard.classfile.attribute.InnerClassesAttribute;
 import proguard.classfile.attribute.InnerClassesInfo;
 import proguard.classfile.attribute.visitor.AttributeVisitor;
 import proguard.classfile.attribute.visitor.InnerClassesInfoVisitor;
-import proguard.classfile.editor.InnerClassesAttributeEditor;
 
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+/**
+ * This {@link AttributeVisitor} and {@link InnerClassesInfoVisitor} removes a given {@link Clazz} from all visited
+ * {@link InnerClassesAttribute}s.
+ *
+ * @author Joren Van Hecke
+ */
 public class InnerClassRemover implements AttributeVisitor, InnerClassesInfoVisitor
 {
     private final Clazz classToBeRemoved;
@@ -26,11 +48,11 @@ public class InnerClassRemover implements AttributeVisitor, InnerClassesInfoVisi
     }
 
     @Override
-    public void visitAnyAttribute(Clazz clazz, Attribute attribute) {
-    }
+    public void visitAnyAttribute(Clazz clazz, Attribute attribute) {}
 
     @Override
-    public void visitInnerClassesAttribute(Clazz clazz, InnerClassesAttribute innerClassesAttribute) {
+    public void visitInnerClassesAttribute(Clazz clazz, InnerClassesAttribute innerClassesAttribute)
+    {
         innerClassesAttribute.innerClassEntriesAccept(clazz, this);
         InnerClassesAttributeEditor editor = new InnerClassesAttributeEditor(innerClassesAttribute);
         logger.trace("{} inner class entries are removed from class {}",
@@ -42,7 +64,8 @@ public class InnerClassRemover implements AttributeVisitor, InnerClassesInfoVisi
     }
 
     @Override
-    public void visitInnerClassesInfo(Clazz clazz, InnerClassesInfo innerClassesInfo) {
+    public void visitInnerClassesInfo(Clazz clazz, InnerClassesInfo innerClassesInfo)
+    {
         String innerClassName = clazz.getClassName(innerClassesInfo.u2innerClassIndex);
         if (Objects.equals(innerClassName, this.classToBeRemoved.getName()))
         {
