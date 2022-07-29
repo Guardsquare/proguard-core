2e58a5c86ec890888782e4b0ea1f5c84d59174f5
James Hamilton <james.hamilton@guardsquare.com>
2022-07-29 16:52:34 +0200

    Update test code

---
+++ android/src/testFixtures/kotlin/ClassPoolBuilder.kt
@@ -0,0 +1,26 @@
+import org.jf.smali.Smali
+import org.jf.smali.SmaliOptions
+import proguard.util.JarUtil
+import testutils.ClassPoolBuilder
+import testutils.ClassPools
+import java.io.File
+
+fun ClassPoolBuilder.Companion.fromSmali(smali: SmaliSource): ClassPools {
+    // smali -> baksmali -> dex file -> dex2pro -> class -> JavaSource
+
+    val file = File.createTempFile("tmp", null)
+    file.writeText(smali.contents)
+
+    val options = SmaliOptions()
+    val dexFile = File.createTempFile("classes", ".dex")
+    val dexFileName = dexFile.absolutePath
+    options.outputDexFile = dexFileName
+    Smali.assemble(options, file.absolutePath)
+
+    file.deleteOnExit()
+    dexFile.deleteOnExit()
+
+    val classPool = JarUtil.readJar(dexFileName, false)
+    initialize(classPool, false)
+    return ClassPools(classPool, libraryClassPool)
+}
