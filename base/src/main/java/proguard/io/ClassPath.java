ecc827fd017405186c316850b0d46227a7370b85
Pramitha Fernando <pramitha.fernando@guardsquare.com>
2022-09-01 10:40:37 +0200

    Add class `ClassPath`

---
+++ base/src/main/java/proguard/io/ClassPath.java
@@ -0,0 +1,90 @@
+/*
+ * ProGuardCORE -- library to process Java bytecode.
+ *
+ * Copyright (c) 2002-2022 Guardsquare NV
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package proguard.io;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * This class represents a class path, as a list of ClassPathEntry objects.
+ *
+ * @author Eric Lafortune
+ */
+public class ClassPath
+{
+    private final List<ClassPathEntry> classPathEntries = new ArrayList<>();
+
+
+    /**
+     * Returns whether the class path contains any output entries.
+     */
+    public boolean hasOutput()
+    {
+        for (ClassPathEntry classPathEntry : classPathEntries) {
+            if (classPathEntry.isOutput()) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
+    // Delegates to List.
+
+    public void clear()
+    {
+        classPathEntries.clear();
+    }
+
+    public void add(int index, ClassPathEntry classPathEntry)
+    {
+        classPathEntries.add(index, classPathEntry);
+    }
+
+    public boolean add(ClassPathEntry classPathEntry)
+    {
+        return classPathEntries.add(classPathEntry);
+    }
+
+    public boolean addAll(ClassPath classPath)
+    {
+        return classPathEntries.addAll(classPath.classPathEntries);
+    }
+
+    public ClassPathEntry get(int index)
+    {
+        return classPathEntries.get(index);
+    }
+
+    public ClassPathEntry remove(int index)
+    {
+        return classPathEntries.remove(index);
+    }
+
+    public boolean isEmpty()
+    {
+        return classPathEntries.isEmpty();
+    }
+
+    public int size()
+    {
+        return classPathEntries.size();
+    }
+}
