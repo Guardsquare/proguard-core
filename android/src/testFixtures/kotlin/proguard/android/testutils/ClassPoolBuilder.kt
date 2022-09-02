cfb7bd1f95bcae564045b603e11b33dbf7d35bed
Pramitha Fernando <pramitha.fernando@guardsquare.com>
2022-09-02 15:42:06 +0200

    Fix issue in `Smali2DexReader`

--- android/src/testFixtures/kotlin/proguard/android/testutils/ClassPoolBuilder.kt
+++ android/src/testFixtures/kotlin/proguard/android/testutils/ClassPoolBuilder.kt
@@ -11,13 +11,16 @@ import proguard.io.util.IOUtil
 import proguard.testutils.ClassPoolBuilder
 import proguard.testutils.ClassPools
 import java.io.File
+import java.io.FileOutputStream
+
+import java.io.InputStream
 
 fun ClassPoolBuilder.Companion.fromSmali(smali: SmaliSource): ClassPools {
     // smali -> baksmali -> dex file -> dex2pro -> class -> JavaSource
     val file = File.createTempFile("tmp", ".smali")
     file.writeText(smali.contents)
 
-    val classPool = IOUtil.read(file.absolutePath, "**", false) { dataEntryReader, classPoolFiller ->
+    val classPool = IOUtil.read(file, false) { dataEntryReader, classPoolFiller ->
         // Convert dex files
         val dexReader = NameFilteredDataEntryReader(
             "classes*.dex",
@@ -41,13 +44,25 @@ fun ClassPoolBuilder.Companion.fromSmali(smali: SmaliSource): ClassPools {
 
 class Smali2DexReader(private val delegate: DataEntryReader) : DataEntryReader {
     override fun read(dataEntry: DataEntry) {
-
         val options = SmaliOptions()
         val dexFile = File.createTempFile("classes", ".dex")
         options.outputDexFile = dexFile.absolutePath
-        Smali.assemble(options, (dataEntry as FileDataEntry).file.absolutePath)
+        val tempFile = File.createTempFile("smali", ".smali")
+        copyInputStreamToFile(dataEntry.inputStream, tempFile)
+        Smali.assemble(options, tempFile.absolutePath)
         val fileDataEntry = FileDataEntry(dexFile)
         delegate.read(fileDataEntry)
         dexFile.deleteOnExit()
+        dataEntry.closeInputStream()
+    }
+
+    private fun copyInputStreamToFile(inputStream: InputStream, file: File) {
+        FileOutputStream(file, false).use { outputStream ->
+            var read: Int
+            val bytes = ByteArray(DEFAULT_BUFFER_SIZE)
+            while (inputStream.read(bytes).also { read = it } != -1) {
+                outputStream.write(bytes, 0, read)
+            }
+        }
     }
 }
