e405a92e82a1b8614fecb69762881a3278f84c7c
Toon Willemot <toon.willemot@guardsquare.com>
2023-03-21 12:03:30 +0100

    Use binary format for ProcessingFlags bitmasks

--- base/src/main/java/proguard/util/ProcessingFlags.java
+++ base/src/main/java/proguard/util/ProcessingFlags.java
@@ -20,15 +20,47 @@ package proguard.util;
 /**
  * Constants used by ProGuard for marking entities (classes, class members,
  * resource files, ...) during processing.
- *
- * @author Johan Leys
  */
 public class ProcessingFlags
 {
+    private static final int mask1  = 0b1;                                // 1
+    private static final int mask2  = 0b10;                               // 2
+    private static final int mask3  = 0b100;                              // 4
+    private static final int mask4  = 0b1000;                             // 8
+    private static final int mask5  = 0b10000;                            // 16
+    private static final int mask6  = 0b100000;                           // 32
+    private static final int mask7  = 0b1000000;                          // 64
+    private static final int mask8  = 0b10000000;                         // 128
+    private static final int mask9  = 0b100000000;                        // 256
+    private static final int mask10 = 0b1000000000;                       // 512
+    private static final int mask11 = 0b10000000000;                      // 1024
+    private static final int mask12 = 0b100000000000;                     // 2048
+    private static final int mask13 = 0b1000000000000;                    // 4096
+    private static final int mask14 = 0b10000000000000;                   // 8192
+    private static final int mask15 = 0b100000000000000;                  // 16384
+    private static final int mask16 = 0b1000000000000000;                 // 32768
+    private static final int mask17 = 0b10000000000000000;                // 65536
+    private static final int mask18 = 0b100000000000000000;               // 131072
+    private static final int mask19 = 0b1000000000000000000;              // 262144
+    private static final int mask20 = 0b10000000000000000000;             // 524288
+    private static final int mask21 = 0b100000000000000000000;            // 1048576
+    private static final int mask22 = 0b1000000000000000000000;           // 2097152
+    private static final int mask23 = 0b10000000000000000000000;          // 4194304
+    private static final int mask24 = 0b100000000000000000000000;         // 8388608
+    private static final int mask25 = 0b1000000000000000000000000;        // 16777216
+    private static final int mask26 = 0b10000000000000000000000000;       // 33554432
+    private static final int mask27 = 0b100000000000000000000000000;      // 67108864
+    private static final int mask28 = 0b1000000000000000000000000000;     // 134217728
+    private static final int mask29 = 0b10000000000000000000000000000;    // 268435456
+    private static final int mask30 = 0b100000000000000000000000000000;   // 536870912
+    private static final int mask31 = 0b1000000000000000000000000000000;  // 1073741824
+    private static final int mask32 = 0b10000000000000000000000000000000; // -2147483648
+
+
     // External configuration flags.
-    public static final int DONT_SHRINK                          = 0x00100000; // Marks whether an entity should not be shrunk.
-    public static final int DONT_OPTIMIZE                        = 0x00200000; // Marks whether an entity should not be optimized.
-    public static final int DONT_OBFUSCATE                       = 0x00400000; // Marks whether an entity should not be obfuscated.
+    public static final int DONT_SHRINK                          = mask21; // Marks whether an entity should not be shrunk.
+    public static final int DONT_OPTIMIZE                        = mask22; // Marks whether an entity should not be optimized.
+    public static final int DONT_OBFUSCATE                       = mask23; // Marks whether an entity should not be obfuscated.
 
     // Combined flags
     public static final int DONT_SHRINK_OR_OBFUSCATE             = DONT_SHRINK    | DONT_OBFUSCATE;
@@ -38,18 +70,18 @@ public class ProcessingFlags
 
 
     // Internal processing flags.
-    public static final int IS_CLASS_AVAILABLE                   = 0x00000001; // Marks whether a class member can be used for generalization or specialization.
-    public static final int REMOVED_FIELDS                       = 0x00000004; // Marks whether a class has (at least one) fields removed.
-    public static final int REMOVED_PUBLIC_FIELDS                = 0x00000008; // Marks whether a class has (at least one) public fields removed.
-    public static final int REMOVED_CONSTRUCTORS                 = 0x00000010; // Marks whether a class has (at least one) constructors removed.
-    public static final int REMOVED_PUBLIC_CONSTRUCTORS          = 0x00000020; // Marks whether a class has (at least one) public constructors removed.
-    public static final int REMOVED_METHODS                      = 0x00000040; // Marks whether a class has (at least one) methods removed.
-    public static final int REMOVED_PUBLIC_METHODS               = 0x00000080; // Marks whether a class has (at least one) public methods removed.
-    public static final int INJECTED                             = 0x00000200; // Marks whether an entity was injected.
-    public static final int DONT_PROCESS_KOTLIN_MODULE           = 0x00008000; // Marks whether to processing a Kotlin module file.
-    public static final int MODIFIED                             = 0x00004000; // Marks whether an entity has been modified.
+    public static final int IS_CLASS_AVAILABLE                   = mask1;  // Marks whether a class member can be used for generalization or specialization.
+    public static final int REMOVED_FIELDS                       = mask3;  // Marks whether a class has (at least one) fields removed.
+    public static final int REMOVED_PUBLIC_FIELDS                = mask4;  // Marks whether a class has (at least one) public fields removed.
+    public static final int REMOVED_CONSTRUCTORS                 = mask5;  // Marks whether a class has (at least one) constructors removed.
+    public static final int REMOVED_PUBLIC_CONSTRUCTORS          = mask6;  // Marks whether a class has (at least one) public constructors removed.
+    public static final int REMOVED_METHODS                      = mask7;  // Marks whether a class has (at least one) methods removed.
+    public static final int REMOVED_PUBLIC_METHODS               = mask8;  // Marks whether a class has (at least one) public methods removed.
+    public static final int INJECTED                             = mask10; // Marks whether an entity was injected.
+    public static final int DONT_PROCESS_KOTLIN_MODULE           = mask16; // Marks whether to processing a Kotlin module file.
+    public static final int MODIFIED                             = mask15; // Marks whether an entity has been modified.
 
     // A mask for processing flags that can be copied as well when e.g. inlining a method / merging a class.
     // TODO: needs to be extended, e.g. with OBFUSCATE_CODE.
-    public static final int COPYABLE_PROCESSING_FLAGS            = 0x00000800;
+    public static final int COPYABLE_PROCESSING_FLAGS            = mask12;
 }
