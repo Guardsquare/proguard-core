ed423248e125838c52cf0abee0c57db7cd2aca46
Toon Willemot <toon.willemot@guardsquare.com>
2023-04-05 14:01:36 +0200

    Add latest android versions to AndroidConstants

--- android/src/main/java/proguard/dexfile/AndroidConstants.java
+++ android/src/main/java/proguard/dexfile/AndroidConstants.java
@@ -26,36 +26,40 @@ package proguard.dexfile;
 public class AndroidConstants
 {
     // Android API versions.
-    public static final int ANDROID_B     =  1; // Android 1.0, Base.
-    public static final int ANDROID_B_1_1 =  2; // Android 1.1.
-    public static final int ANDROID_C     =  3; // Android 1.2, Cupcake.
-    public static final int ANDROID_D     =  4; // Android 1.3, Donut.
-    public static final int ANDROID_E     =  5; // Android 2.0, Eclair.
-    public static final int ANDROID_E_0_1 =  6;
-    public static final int ANDROID_E_MR1 =  7;
-    public static final int ANDROID_F     =  8; // Android 2.2, Froyo.
-    public static final int ANDROID_G     =  9; // Android 2.3, Gingerbread.
-    public static final int ANDROID_G_MR1 = 10;
-    public static final int ANDROID_H     = 11; // Android 3.0, Honeycomb.
-    public static final int ANDROID_H_MR1 = 12;
-    public static final int ANDROID_H_MR2 = 13;
-    public static final int ANDROID_I     = 14; // Android 4.0, Ice Cream Sandwich.
-    public static final int ANDROID_I_MR1 = 15;
-    public static final int ANDROID_J     = 16; // Android 4.1, Jelly Bean.
-    public static final int ANDROID_J_MR1 = 17;
-    public static final int ANDROID_J_MR2 = 18;
-    public static final int ANDROID_K     = 19; // Android 4.4, KitKat.
-    public static final int ANDROID_K_MR1 = 20;
-    public static final int ANDROID_L     = 21; // Android 5.0, Lollipop.
-    public static final int ANDROID_L_MR1 = 22;
-    public static final int ANDROID_M     = 23; // Android 6.0, Marshmallow.
-    public static final int ANDROID_N     = 24; // Android 7.0, Nougat.
-    public static final int ANDROID_N_MR1 = 25;
-    public static final int ANDROID_O     = 26; // Android 8.0, Oreo.
-    public static final int ANDROID_O_MR1 = 27;
-    public static final int ANDROID_P     = 28; // Android 9.0.
-    public static final int ANDROID_Q     = 29; // Android 10.0.
-    public static final int ANDROID_R     = 30; // Android 11.0.
+    public static final int ANDROID_B                =  1; // Android 1.0, Base.
+    public static final int ANDROID_B_1_1            =  2; // Android 1.1.
+    public static final int ANDROID_C                =  3; // Android 1.2, Cupcake.
+    public static final int ANDROID_D                =  4; // Android 1.3, Donut.
+    public static final int ANDROID_E                =  5; // Android 2.0, Eclair.
+    public static final int ANDROID_E_0_1            =  6;
+    public static final int ANDROID_E_MR1            =  7;
+    public static final int ANDROID_F                =  8; // Android 2.2, Froyo.
+    public static final int ANDROID_G                =  9; // Android 2.3, Gingerbread.
+    public static final int ANDROID_G_MR1            = 10;
+    public static final int ANDROID_H                = 11; // Android 3.0, Honeycomb.
+    public static final int ANDROID_H_MR1            = 12;
+    public static final int ANDROID_H_MR2            = 13;
+    public static final int ANDROID_I                = 14; // Android 4.0, Ice Cream Sandwich.
+    public static final int ANDROID_I_MR1            = 15;
+    public static final int ANDROID_J                = 16; // Android 4.1, Jelly Bean.
+    public static final int ANDROID_J_MR1            = 17;
+    public static final int ANDROID_J_MR2            = 18;
+    public static final int ANDROID_K                = 19; // Android 4.4, KitKat.
+    public static final int ANDROID_K_MR1            = 20;
+    public static final int ANDROID_L                = 21; // Android 5.0, Lollipop.
+    public static final int ANDROID_L_MR1            = 22;
+    public static final int ANDROID_M                = 23; // Android 6.0, Marshmallow.
+    public static final int ANDROID_N                = 24; // Android 7.0, Nougat.
+    public static final int ANDROID_N_MR1            = 25;
+    public static final int ANDROID_O                = 26; // Android 8.0, Oreo.
+    public static final int ANDROID_O_MR1            = 27;
+    public static final int ANDROID_P                = 28; // Android 9.0.
+    public static final int ANDROID_Q                = 29; // Android 10.0.
+    public static final int ANDROID_R                = 30; // Android 11.0.
+    public static final int ANDROID_S                = 31; // Android 12.0.
+    public static final int ANDROID_S_V2             = 32; // Android 12.0.
+    public static final int ANDROID_TIRAMISU         = 33; // Android 13.0.
+    public static final int ANDROID_UPSIDE_DOWN_CAKE = 34; // Android 14.0.
 
     public static final String ZIP_META_INF_EXPRESSION = "META-INF/**";
 
@@ -106,12 +110,12 @@ public class AndroidConstants
     public static final String AAB_BUNDLETOOL_PREFIX    = "BUNDLE-METADATA/com.android.tools.build.bundletool/";
     public static final String MAIN_DEX_LIST_TXT        = "mainDexList.txt";
 
-    public static final String AAB_ANDROID_MANIFEST_XML_SUFFIX= "/manifest/AndroidManifest.xml";
-    public static final String AAB_RESOURCES_PB_SUFFIX        = "/resources.pb";
-    public static final String AAB_RESOURCES_ARSC_SUFFIX      = "/resources.arsc";
-    public static final String AAB_MANIFEST_INFIX             = "/manifest/";
-    public static final String AAB_DEX_INFIX                  = "/dex/";
-    public static final String AAB_ROOT_INFIX                 = "/root/";
+    public static final String AAB_ANDROID_MANIFEST_XML_SUFFIX = "/manifest/AndroidManifest.xml";
+    public static final String AAB_RESOURCES_PB_SUFFIX         = "/resources.pb";
+    public static final String AAB_RESOURCES_ARSC_SUFFIX       = "/resources.arsc";
+    public static final String AAB_MANIFEST_INFIX              = "/manifest/";
+    public static final String AAB_DEX_INFIX                   = "/dex/";
+    public static final String AAB_ROOT_INFIX                  = "/root/";
 
     public static final String AAB_BUNDLE_METADATA_EXPRESSION      = "BUNDLE-METADATA/**";
     public static final String AAB_ANDROID_MANIFEST_XML_EXPRESSION = "*/manifest/AndroidManifest.xml";
@@ -184,70 +188,6 @@ public class AndroidConstants
 
     public static final String BUNDLE_METADATA_EXPRESSION           = ZIP_META_INF_EXPRESSION + ',' +
                                                                       AAB_BUNDLE_METADATA_EXPRESSION;
-
-    // Type & name constants.
-    public static final String TYPE_ANDROID_CONTENT_CONTEXT                                 = "Landroid/content/Context;";
-    public static final String NAME_ANDROID_CONTENT_CONTEXT                                 = "android/content/Context";
-    public static final String FIELD_NAME_ACTIVITY_SERVICE                                  = "activity";
-    public static final String NAME_ANDROID_CONTEXT_WRAPPER                                 = "android/content/ContextWrapper";
-
-    public static final String METHOD_NAME_ATTACH_BASE_CONTEXT                              = "attachBaseContext";
-    public static final String METHOD_TYPE_ATTACH_BASE_CONTEXT                              = "(" + TYPE_ANDROID_CONTENT_CONTEXT + ")V";
-
-    public static final String METHOD_NAME_ON_CREATE                                        = "onCreate";
-    public static final String METHOD_TYPE_ON_CREATE                                        = "()V";
-
-    public static final String TYPE_ANDROID_CONTENT_RES_RESOURCES                           = "Landroid/content/res/Resources;";
-    public static final String METHOD_NAME_GET_RESOURCES                                    = "getResources";
-    public static final String METHOD_TYPE_GET_RESOURCES                                    = "()" + TYPE_ANDROID_CONTENT_RES_RESOURCES;
-
-    public static final String METHOD_NAME_GET_BASE_CONTEXT                                 = "getBaseContext";
-    public static final String METHOD_TYPE_GET_BASE_CONTEXT                                 = "()" + TYPE_ANDROID_CONTENT_CONTEXT;
-
-    public static final String METHOD_NAME_GET_APPLICATION_CONTEXT                          = "getApplicationContext";
-    public static final String METHOD_TYPE_GET_APPLICATION_CONTEXT                          = "()" + TYPE_ANDROID_CONTENT_CONTEXT;
-
-    public static final String CLASS_NAME_ANDROID_APP_ACTIVITYTHREAD                        = "android.app.ActivityThread";
-    public static final String METHOD_NAME_CURRENTAPPLICATION                               = "currentApplication";
-
-    public static final String TYPE_JAVA_LANG_RUNTIME                                       = "Ljava/lang/Runtime;";
-    public static final String METHOD_NAME_EXEC                                             = "exec";
-
-    public static final String NAME_ANDROID_OS_PROCESS                                      = "android/os/Process";
-    public static final String TYPE_ANDROID_OS_PROCESS                                      = "Landroid/os/Process;";
-    public static final String METHOD_NAME_MYPID                                            = "myPid";
-    public static final String METHOD_NAME_MYUID                                            = "myUid";
-    public static final String METHOD_NAME_KILLPROCESS                                      = "killProcess";
-
-    public static final String METHOD_NAME_FORMAT                                           = "format";
-
-    public static final String CLASS_NAME_ANDROID_OS_BUILD_VERSION                          = "android.os.Build$VERSION";
-    public static final String FIELD_NAME_SDK_INT                                           = "SDK_INT";
-    public static final String FIELD_TYPE_SDK_INT                                           = "I";
-
-    public static final String METHOD_NAME_GET_SYSTEM_SERVICE                               = "getSystemService";
-
-    public static final String CLASS_NAME_ANDROID_APP_ACTIVITYMANAGER                       = "android.app.ActivityManager";
-    public static final String METHOD_NAME_GETAPPTASKS                                      = "getAppTasks";
-    public static final String METHOD_NAME_GETRUNNINGAPPPROCESSES                           = "getRunningAppProcesses";
-
-    public static final String CLASS_NAME_ANDROID_APP_APPLICATION                           = "android.app.Application";
-
-    public static final String CLASS_NAME_ANDROID_APP_ACTIVITYMANAGER_APPTASK               = "android.app.ActivityManager$AppTask";
-    public static final String CLASS_NAME_ANDROID_APP_ACTIVITYMANAGER_RUNNINGAPPPROCESSINFO = "android.app.ActivityManager$RunningAppProcessInfo";
-    public static final String METHOD_NAME_FINISHANDREMOVETASK                              = "finishAndRemoveTask";
-    public static final String FIELD_NAME_PID                                               = "pid";
-
-    public static final String FIELD_NAME_ITERATOR                                          = "iterator";
-    public static final String FIELD_TYPE_ITERATOR                                          = "()Ljava/util/Iterator;";
-
-    public static final String NAME_JAVA_UTIL_ITERATOR                                      = "java/util/Iterator";
-    public static final String METHOD_NAME_HASNEXT                                          = "hasNext";
-    public static final String METHOD_TYPE_HASNEXT                                          = "()Z";
-    public static final String METHOD_NAME_NEXT                                             = "next";
-    public static final String METHOD_TYPE_NEXT                                             = "()Ljava/lang/Object;";
-
-
     // Resource paths.
     public static final String RES_FONT           = "res/font*/*";
     public static final String RES_DRAWABLE       = "res/drawable**";
