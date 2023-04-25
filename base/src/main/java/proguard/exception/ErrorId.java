3e99686675446dbb4733a8a31ce1c1731a773bcf
younes <younes.zeboudj@guardsquare.com>
2023-04-25 16:15:46 +0200

    Make ProguardCoreException public

--- base/src/main/java/proguard/exception/ErrorId.java
+++ base/src/main/java/proguard/exception/ErrorId.java
@@ -3,7 +3,7 @@ package proguard.exception;
 /**
  * Class holding all the error ids for exceptions occurring in the program.
  */
-final class ErrorId {
+public final class ErrorId {
 
     /**
      * Private constructor to prevent instantiation of the class.
