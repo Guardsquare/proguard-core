c96b64f7cd53519997302ee2a2f72ad1af66a412
younes <younes.zeboudj@guardsquare.com>
2023-04-27 15:55:36 +0200

    Add test id in ErrorId class

--- base/src/main/java/proguard/exception/ErrorId.java
+++ base/src/main/java/proguard/exception/ErrorId.java
@@ -5,6 +5,8 @@ package proguard.exception;
  */
 public final class ErrorId {
 
+    // this id is for testing purposes, to be removed at the end of T20409
+    public static int TEST_ID= 99;
     /**
      * Private constructor to prevent instantiation of the class.
      */
