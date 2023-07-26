84d2cf13a671241b89b74aa191a897d9a7fecc85
MaartenS <maarten.steevens@gmail.com>
2023-07-26 14:48:32 +0200

    Use slice to limit the search to u2attributesCount

--- base/src/testFixtures/kotlin/proguard/testutils/PartialEvaluatorUtil.kt
+++ base/src/testFixtures/kotlin/proguard/testutils/PartialEvaluatorUtil.kt
@@ -26,9 +26,9 @@ class PartialEvaluatorUtil {
             val method = clazz.findMethod(methodName, methodDescriptor) as ProgramMethod
 
             val codeAttribute =
-                method.attributes.find { it?.getAttributeName(clazz) == Attribute.CODE } as CodeAttribute
+                method.attributes.slice(0 until method.u2attributesCount).find { it.getAttributeName(clazz) == Attribute.CODE } as CodeAttribute
             val localVarTableAttribute =
-                codeAttribute.attributes.find { it?.getAttributeName(clazz) == Attribute.LOCAL_VARIABLE_TABLE } as LocalVariableTableAttribute?
+                codeAttribute.attributes.slice(0 until codeAttribute.u2attributesCount).find { it.getAttributeName(clazz) == Attribute.LOCAL_VARIABLE_TABLE } as LocalVariableTableAttribute?
 
             val instructions = ArrayList<Pair<Int, Instruction>>()
             codeAttribute.instructionsAccept(
