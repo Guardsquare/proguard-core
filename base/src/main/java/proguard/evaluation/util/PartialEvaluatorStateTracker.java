680dcce5261c08ac1b8decb6a6fe0e3ff2ff0160
Jitse De Smet <jitse@reedonk8.be>
2023-07-31 12:47:51 +0200

    fix: general issue that has been here for a while I guess?

--- base/src/main/java/proguard/evaluation/util/PartialEvaluatorStateTracker.java
+++ base/src/main/java/proguard/evaluation/util/PartialEvaluatorStateTracker.java
@@ -176,11 +176,6 @@ public interface PartialEvaluatorStateTracker
     default void registerSubroutineReturn(Clazz clazz, Method method, int returnOffset, TracedVariables returnVariables, TracedStack returnStack) {}
 
     /**
-     * The current instruction was RET and the partial evaluator pushes the return address to the branch stack of the calling partial evaluator
-     */
-    void registerSubroutineReturn(Clazz clazz, Method method, int returnOffset, TracedVariables returnVariables, TracedStack returnStack);
-
-    /**
      * The partial evaluator will start generalizing the results of the evaluated subroutine.
      */
     default void generalizeSubroutine(Clazz clazz, Method method, TracedVariables startVariables, TracedStack startStack, int subroutineStart, int subroutineEnd) {}
