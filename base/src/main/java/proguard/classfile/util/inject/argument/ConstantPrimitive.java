392a1f1ed020789950acb8d363c5646d9c01ddc1
Kymeng <137695428+gnemyk@users.noreply.github.com>
2024-12-04 20:47:02 +0100

    Lint with spotless

--- base/src/main/java/proguard/classfile/util/inject/argument/ConstantPrimitive.java
+++ base/src/main/java/proguard/classfile/util/inject/argument/ConstantPrimitive.java
@@ -3,41 +3,49 @@ package proguard.classfile.util.inject.argument;
 import proguard.classfile.util.ClassUtil;
 
 /**
- * A model representing a constant value of primitive typed argument to be passed to the method invocation instructions
- * that are injected by {@link proguard.classfile.util.inject.CodeInjector}.
+ * A model representing a constant value of primitive typed argument to be passed to the method
+ * invocation instructions that are injected by {@link proguard.classfile.util.inject.CodeInjector}.
  *
  * @author Kymeng Tang
  */
 public class ConstantPrimitive<T extends Number> implements InjectedArgument {
-    private final T numericConstant;
-    public ConstantPrimitive(T constant) {
-        this.numericConstant = constant;
-    }
+  private final T numericConstant;
 
-    @Override
-    public Object getValue() {
-        return numericConstant;
-    }
+  public ConstantPrimitive(T constant) {
+    this.numericConstant = constant;
+  }
 
-    @Override
-    public String getInternalType() {
-        switch (numericConstant.getClass().getName())
-        {
-            case "java.lang.Boolean":   return "Z";
-            case "java.lang.Byte":      return "B";
-            case "java.lang.Character": return "C";
-            case "java.lang.Short":     return "S";
-            case "java.lang.Integer":   return "I";
-            case "java.lang.Long":      return "J";
-            case "java.lang.Float":     return "F";
-            case "java.lang.Double":    return "D";
-            default:
-                throw new RuntimeException("Unexpected type");
-        }
-    }
+  @Override
+  public Object getValue() {
+    return numericConstant;
+  }
 
-    @Override
-    public String toString() {
-        return numericConstant.toString() + ":" + ClassUtil.externalType(getInternalType());
+  @Override
+  public String getInternalType() {
+    switch (numericConstant.getClass().getName()) {
+      case "java.lang.Boolean":
+        return "Z";
+      case "java.lang.Byte":
+        return "B";
+      case "java.lang.Character":
+        return "C";
+      case "java.lang.Short":
+        return "S";
+      case "java.lang.Integer":
+        return "I";
+      case "java.lang.Long":
+        return "J";
+      case "java.lang.Float":
+        return "F";
+      case "java.lang.Double":
+        return "D";
+      default:
+        throw new RuntimeException("Unexpected type");
     }
+  }
+
+  @Override
+  public String toString() {
+    return numericConstant.toString() + ":" + ClassUtil.externalType(getInternalType());
+  }
 }
--- base/src/main/java/proguard/classfile/util/inject/argument/InjectedArgument.java
+++ base/src/main/java/proguard/classfile/util/inject/argument/InjectedArgument.java
@@ -1,15 +1,15 @@
 package proguard.classfile.util.inject.argument;
 
 /**
- * This interface defines api for modeling arguments to be passed to the method invocation instructions that are
- * injected by {@link proguard.classfile.util.inject.CodeInjector}.
+ * This interface defines api for modeling arguments to be passed to the method invocation
+ * instructions that are injected by {@link proguard.classfile.util.inject.CodeInjector}.
  *
  * @author Kymeng Tang
  */
 public interface InjectedArgument {
-    // A getter that returns a boxed value of the argument.
-    Object getValue();
+  // A getter that returns a boxed value of the argument.
+  Object getValue();
 
-    // A getter indicating the internal JVM type that describes the argument.
-    String getInternalType();
+  // A getter indicating the internal JVM type that describes the argument.
+  String getInternalType();
 }
